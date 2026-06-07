861767383796fa433a3273cbd50063c989e56528
usekeeee <afnysoat1207@gmail.com>
2026-06-07 23:37:38 +0900

    2章の勉強

---
+++ src/chap2/Account.java
@@ -0,0 +1,8 @@
+package chap2;
+
+public class Account {
+    private String accountNo;
+    private int zandaka;
+    private AccountType accountType;
+    public Account(String aNo, AccountType aType){}
+}
---
+++ src/chap2/Pocket.java
@@ -0,0 +1,7 @@
+package chap2;
+
+public class Pocket<E>{
+    private E data;
+    public void put(E d){this.data = d;}
+    public E get() {return  this.data;}
+}
